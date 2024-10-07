4baa6e9696a123e8806d33daff6072928728436a
ucangun <149247682+ucangun@users.noreply.github.com>
2024-10-07 23:40:33 +0200

    "Added data reading from file and API endpoint to server"

--- 01_node-farm/index.js
+++ 01_node-farm/index.js
@@ -33,6 +33,10 @@ const url = require("url");
 
 /////////////////////////////////
 // SERVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   console.log(req.url);
 
@@ -42,6 +46,9 @@ const server = http.createServer((req, res) => {
     res.end("This is the Overview");
   } else if (pathName === "/product") {
     res.end("This is the Product");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(data);
   } else {
     res.writeHead(404, {
       "Content-Type": "text/html",
