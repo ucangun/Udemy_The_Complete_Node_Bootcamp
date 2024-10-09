393da9dbe30ae8d2ad88f0b93ed38d9303bd309e
ucangun <149247682+ucangun@users.noreply.github.com>
2024-10-09 14:30:39 +0200

    "Added three template files (template-overview.html, template-card.html, template-product.html) to be read by fs.readFileSync"

--- 01_node-farm/index.js
+++ 01_node-farm/index.js
@@ -33,6 +33,20 @@ const url = require("url");
 
 /////////////////////////////////
 // SERVER
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
