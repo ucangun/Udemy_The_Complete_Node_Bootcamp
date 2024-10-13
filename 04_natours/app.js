5ee8c2d2c16663d27898ece40c65ce15b96a2a01
ucangun <149247682+ucangun@users.noreply.github.com>
2024-10-13 23:59:22 +0200

    "Added morgan middleware, organized code into sections (middlewares, route handlers, routes, start server), and reformatted code"

--- 04_natours/app.js
+++ 04_natours/app.js
@@ -1,7 +1,12 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+
+// 1) MIDDLEWARES
+app.use(morgan('dev'));
+
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -17,6 +22,8 @@ app.use((req, res, next) => {
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
+
+// 2) ROUTE HANDLERS
 
 const getAllTours = (req, res) => {
   console.log(req.requestTime);
@@ -100,6 +107,8 @@ const deleteTour = (req, res) => {
   });
 };
 
+// 3) ROUTES
+
 // app.get('/api/v1/tours', getAllTours);
 // app.post('/api/v1/tours', createTour);
 // app.get('/api/v1/tours/:id', getTour);
@@ -113,6 +122,8 @@ app
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+// 4) START SERVER
 
 const port = 3000;
 app.listen(port, () => {
