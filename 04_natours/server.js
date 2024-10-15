a05ef1b4c5e8216f053eecb71a9baafe4a222045
ucangun <149247682+ucangun@users.noreply.github.com>
2024-10-15 15:01:32 +0200

    db connection with app

--- 04_natours/server.js
+++ 04_natours/server.js
@@ -1,10 +1,22 @@
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
-
 const app = require('./app');
 
-// console.log(process.env);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB)
+  .then(() => console.log('DB connection successful!'))
+  .catch((err) => {
+    console.error('DB connection error:', err.message);
+    process.exit(1);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
