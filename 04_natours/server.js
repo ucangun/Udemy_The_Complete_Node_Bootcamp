69c55e027c57dc5d7ea49d9c59632b2eb74445e4
ucangun <149247682+ucangun@users.noreply.github.com>
2024-10-15 16:07:36 +0200

    "Added tour schema and model, created a test tour instance, and saved it to the database."

--- 04_natours/server.js
+++ 04_natours/server.js
@@ -15,7 +15,39 @@ mongoose
   .then(() => console.log('DB connection successful!'))
   .catch((err) => {
     console.error('DB connection error:', err.message);
-    process.exit(1);
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  rating: 4.2,
+  price: 495,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.error(err.message);
   });
 
 const port = process.env.PORT || 3000;
