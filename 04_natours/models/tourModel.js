a2a811b8441c8c0c9f47e07d92e92ee61d7f0d54
ucangun <149247682+ucangun@users.noreply.github.com>
2024-10-16 16:45:12 +0200

    "Added query middleware to tourModel.js to filter out secret tours and log query execution time"

--- 04_natours/models/tourModel.js
+++ 04_natours/models/tourModel.js
@@ -98,6 +98,21 @@ tourSchema.pre('save', function (next) {
 //   next();
 // });
 
+// QUERY MIDDLEWARE
+
+// tourSchema.pre('find', function(next) {
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  next();
+});
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
