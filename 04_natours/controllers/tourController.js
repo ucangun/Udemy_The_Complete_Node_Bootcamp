a98c7bb4a97c8a384359926758fe2a8d02b0a7ff
ucangun <149247682+ucangun@users.noreply.github.com>
2024-10-16 11:35:28 +0200

    "Added pagination to getAllTours controller"

--- 04_natours/controllers/tourController.js
+++ 04_natours/controllers/tourController.js
@@ -69,15 +69,23 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // 4) Pagination
+
+    // page=2&limit=10
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     // EXECUTE QUERY
 
     const tours = await query;
-
-    // const query = await Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('difficulty')
-    //   .equals(5);
 
     // SEND RESPONSE
 
