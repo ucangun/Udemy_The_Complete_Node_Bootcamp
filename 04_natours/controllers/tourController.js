1fd9f4c6147a061c5e3b6f3e2591d288cc06ad57
ucangun <149247682+ucangun@users.noreply.github.com>
2024-10-16 10:54:01 +0200

    "Added sorting functionality to tourController.js"

--- 04_natours/controllers/tourController.js
+++ 04_natours/controllers/tourController.js
@@ -34,20 +34,31 @@ exports.getAllTours = async (req, res) => {
     console.log(req.query);
     // BUILD QUERY
 
-    // 1) Filtering
+    // 1A) Filtering
 
     const queryObj = { ...req.query };
     const excludeFields = ['page', 'sort', 'limit', 'fields'];
     excludeFields.forEach((el) => delete queryObj[el]);
 
-    // 2) Advanced Filtering
+    // 2B) Advanced Filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
     console.log(JSON.parse(queryStr));
 
     // {difficulty:'easy , duration : {$gte:5} }
 
-    const query = await Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) SORTING
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      console.log(sortBy);
+
+      query = query.sort(sortBy);
+      // sort('price', ratingsAverage)
+    } else {
+      query = query.sort('-createdAt');
+    }
     // EXECUTE QUERY
 
     const tours = await query;
