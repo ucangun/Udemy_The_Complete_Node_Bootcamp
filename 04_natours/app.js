1f06e116dc285f34f6c69c942a4a0d3c1a09d9c4
ucangun <149247682+ucangun@users.noreply.github.com>
2024-11-05 23:31:21 +0100

    "Added express-rate-limit middleware to app.js and updated package.json and package-lock.json files"

--- 04_natours/app.js
+++ 04_natours/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 const dotenv = require('dotenv');
 
 const AppError = require('./utils/appError');
@@ -11,10 +12,20 @@ dotenv.config();
 
 const app = express();
 
-// 1) MIDDLEWARES
+// 1) GLOBAL MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
+
+/*******************************************************************/
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
@@ -24,6 +35,8 @@ app.use((req, res, next) => {
   console.log(req.headers);
   next();
 });
+
+/*******************************************************************/
 
 // 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
