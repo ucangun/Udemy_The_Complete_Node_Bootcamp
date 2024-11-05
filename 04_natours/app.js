8721432e4242f718bf19b9adad9c7f13356dad31
ucangun <149247682+ucangun@users.noreply.github.com>
2024-11-05 23:48:04 +0100

    "Removed dotenv config, added hpp middleware, and updated package dependencies"

--- 04_natours/app.js
+++ 04_natours/app.js
@@ -2,21 +2,18 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
-const dotenv = require('dotenv');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
-dotenv.config();
-
 const app = express();
 
 // 1) GLOBAL MIDDLEWARES
-
 // Set security HTTP headers
 app.use(helmet());
 
@@ -33,8 +30,6 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-/*******************************************************************/
-
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
 
@@ -43,6 +38,20 @@ app.use(mongoSanitize());
 
 // Data sanitization against XSS
 app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
@@ -53,8 +62,6 @@ app.use((req, res, next) => {
   // console.log(req.headers);
   next();
 });
-
-/*******************************************************************/
 
 // 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
