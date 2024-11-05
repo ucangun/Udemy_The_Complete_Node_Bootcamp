5c49663d3f63b285cfc874a1b315d598a3dd18c3
ucangun <149247682+ucangun@users.noreply.github.com>
2024-11-05 22:23:53 +0100

    "Added deleteMe functionality to userController and userRoutes"

--- 04_natours/routes/userRoutes.js
+++ 04_natours/routes/userRoutes.js
@@ -18,6 +18,8 @@ router.patch(
 
 router.patch('/updateMe', authController.protect, userController.updateMe);
 
+router.delete('/deleteMe', authController.protect, userController.deleteMe);
+
 router
   .route('/')
   .get(userController.getAllUsers)
