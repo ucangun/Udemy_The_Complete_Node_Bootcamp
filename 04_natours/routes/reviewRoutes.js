36eb7b4eae2a51e6df0f94935e6f4d5c3460047b
ucangun <149247682+ucangun@users.noreply.github.com>
2024-11-12 22:08:23 +0100

    "Added reviewRouter to app.js and mounted it at '/api/v1/reviews'"

---
+++ 04_natours/routes/reviewRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const reviewController = require('./../controllers/reviewController');
+const authController = require('./../controllers/authController');
+
+const router = express.Router({ mergeParams: true });
+
+router.use(authController.protect);
+
+router
+  .route('/')
+  .get(reviewController.getAllReviews)
+  .post(authController.restrictTo('user'), reviewController.createReview);
+
+module.exports = router;
